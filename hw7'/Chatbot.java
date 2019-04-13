283d19f404b176fd8017621c95bc2d22c9a97231
Rui yin <yaluaiace@gmail.com>
2019-04-12 21:31:42 -0500

    Add files via upload

---
+++ hw7'/Chatbot.java
@@ -0,0 +1,117 @@
+
+import java.util.*;
+import java.io.*;
+
+public class Chatbot{
+    private static String filename = "./corpus.txt";
+    private static ArrayList<Integer> readCorpus(){
+        ArrayList<Integer> corpus = new ArrayList<Integer>();
+        try{
+            File f = new File(filename);
+            Scanner sc = new Scanner(f);
+            while(sc.hasNext()){
+                if(sc.hasNextInt()){
+                    int i = sc.nextInt();
+                    corpus.add(i);
+                }
+                else{
+                    sc.next();
+                }
+            }
+        }
+        catch(FileNotFoundException ex){
+            System.out.println("File Not Found.");
+        }
+        return corpus;
+    }
+    static public void main(String[] args){
+        ArrayList<Integer> corpus = readCorpus();
+		int flag = Integer.valueOf(args[0]);
+        
+        if(flag == 100){
+			int w = Integer.valueOf(args[1]);
+            int count = 0;
+            //TODO count occurence of w
+            
+        }
+        else if(flag == 200){
+            int n1 = Integer.valueOf(args[1]);
+            int n2 = Integer.valueOf(args[2]);
+            //TODO generate
+
+        }
+        else if(flag == 300){
+            int h = Integer.valueOf(args[1]);
+            int w = Integer.valueOf(args[2]);
+            int count = 0;
+            ArrayList<Integer> words_after_h = new ArrayList<Integer>();
+            //TODO
+
+        }
+        else if(flag == 400){
+            int n1 = Integer.valueOf(args[1]);
+            int n2 = Integer.valueOf(args[2]);
+            int h = Integer.valueOf(args[3]);
+            //TODO
+            
+        }
+        else if(flag == 500){
+            int h1 = Integer.valueOf(args[1]);
+            int h2 = Integer.valueOf(args[2]);
+            int w = Integer.valueOf(args[3]);
+            int count = 0;
+            ArrayList<Integer> words_after_h1h2 = new ArrayList<Integer>();
+            //TODO
+			
+        }
+        else if(flag == 600){
+            int n1 = Integer.valueOf(args[1]);
+            int n2 = Integer.valueOf(args[2]);
+            int h1 = Integer.valueOf(args[3]);
+            int h2 = Integer.valueOf(args[4]);
+            //TODO
+        }
+        else if(flag == 700){
+            int seed = Integer.valueOf(args[1]);
+            int t = Integer.valueOf(args[2]);
+            int h1=0,h2=0;
+
+            Random rng = new Random();
+            if (seed != -1) rng.setSeed(seed);
+
+            if(t == 0){
+                // TODO Generate first word using r
+                double r = rng.nextDouble();
+                System.out.println(h1);
+                if(h1 == 9 || h1 == 10 || h1 == 12){
+                    return;
+                }
+
+                // TODO Generate second word using r
+                r = rng.nextDouble();
+                System.out.println(h2);
+            }
+            else if(t == 1){
+                h1 = Integer.valueOf(args[3]);
+                // TODO Generate second word using r
+                double r = rng.nextDouble();
+                System.out.println(h2);
+            }
+            else if(t == 2){
+                h1 = Integer.valueOf(args[3]);
+                h2 = Integer.valueOf(args[4]);
+            }
+
+            while(h2 != 9 && h2 != 10 && h2 != 12){
+                double r = rng.nextDouble();
+                int w  = 0;
+                // TODO Generate new word using h1,h2
+                System.out.println(w);
+                h1 = h2;
+                h2 = w;
+            }
+        }
+
+        return;
+    }
+}
